d8cf81579b3d3c573541aba6e3366a5c6c8fcfdc
Eugene Efimochkin <eugene@aboutecho.com>
2014-08-22 14:10:35 +0400

    Added image app

---
+++ src/image/app.js
@@ -0,0 +1,55 @@
+(function ($) {
+"use strict";
+
+var imageApp = Echo.App.manifest("Echo.Apps.Image");
+
+if (Echo.App.isDefined("Echo.Apps.Image")) {
+	return;
+};
+
+imageApp.templates.main = '<image class={class:image}>';
+
+imageApp.renderers.image = function (element) {
+	var targetRect = [this.config.get("width"), this.config.get("height")];
+	element.attr('src', this.config.get('imageURL'));
+
+	// determining the real image size to properly scale it
+	element
+		.clone()
+		.load(function() {
+			var fitWidth = function (imgSize, rect) {
+				var widths = [imgSize[0]],
+					width;
+				for (var i = 0; i < 2; i++) {
+					if (rect[i] > 0 ) {
+						width = imgSize[0] * rect[i] / imgSize[i];
+						if ((width > 0)) {
+							widths.push(width);
+						}
+					}
+				}
+				return Math.min.apply(null, widths);
+			};
+
+			element.attr("width", "" + fitWidth([this.width, this.height], targetRect) + "px");
+		});
+
+  var targetURL = this.config.get("linkURL");
+
+  if (targetURL) {
+  	var a = $("<a></a>").attr("href", targetURL);
+  	element = element.wrap(a);
+  }
+	return element;
+};
+
+
+imageApp.config = {
+	"imageURL": ""
+};
+
+imageApp.css = ".{class} .{class:image} { max-width: 100%; height: auto; }";
+
+Echo.App.create(imageApp);
+
+})(Echo.jQuery);
---
+++ src/image/dashboard.js
@@ -0,0 +1,61 @@
+(function($) {
+"use strict";
+
+if (Echo.AppServer.Dashboard.isDefined("Echo.Apps.Image.Dashboard")) return;
+
+var dashboard = Echo.AppServer.Dashboard.manifest("Echo.Apps.Image.Dashboard");
+
+dashboard.inherits = Echo.Utils.getComponent("Echo.AppServer.Dashboards.AppSettings");
+
+dashboard.config = {
+	"ecl": [{
+		"name": "imageURL",
+		"type": "string",
+		"component": "Input",
+		"default": "0",
+		"config": {
+			"title": "Image URL",
+			"desc": "Specifies URL the image will be loaded from"
+		}, {
+		"name": "heigth",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Height",
+			"desc": "Specifies width of the image"
+		}, {
+		"name": "width",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Width",
+			"desc": "Specifies height of the image"
+		}, {
+		"name": "alt",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Alt text",
+			"desc": "Sets the alt text for image"
+		}, {
+		"name": "linkURL",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Hyperlink URL",
+			"desc": "Sets the URL for hyperlink around the image"
+		}
+	}]
+};
+
+dashboard.init = function() {
+	this.parent();
+};
+
+Echo.AppServer.Dashboard.create(dashboard);
+
+})(Echo.jQuery);
