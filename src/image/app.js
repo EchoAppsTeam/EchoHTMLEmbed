2753de3d2e40988fcc0fe16b84e645180a78c779
Eugene Efimochkin <eugene@aboutecho.com>
2014-08-25 10:55:33 +0400

    Just some formatting

--- src/image/app.js
+++ src/image/app.js
@@ -1,15 +1,13 @@
-(function ($) {
+(function($) {
 "use strict";
 
 var imageApp = Echo.App.manifest("Echo.Apps.Image");
 
-if (Echo.App.isDefined("Echo.Apps.Image")) {
-	return;
-};
+if (Echo.App.isDefined("Echo.Apps.Image")) return;
 
 imageApp.templates.main = '<image class={class:image}>';
 
-imageApp.renderers.image = function (element) {
+imageApp.renderers.image = function(element) {
 	var targetRect = [this.config.get("width"), this.config.get("height")];
 	element.attr('src', this.config.get('imageURL'));
 
@@ -17,11 +15,11 @@ imageApp.renderers.image = function (element) {
 	element
 		.clone()
 		.load(function() {
-			var fitWidth = function (imgSize, rect) {
+			var fitWidth = function(imgSize, rect) {
 				var widths = [imgSize[0]],
 					width;
 				for (var i = 0; i < 2; i++) {
-					if (rect[i] > 0 ) {
+					if (rect[i] > 0) {
 						width = imgSize[0] * rect[i] / imgSize[i];
 						if ((width > 0)) {
 							widths.push(width);
@@ -32,14 +30,15 @@ imageApp.renderers.image = function (element) {
 			};
 
 			element.attr("width", "" + fitWidth([this.width, this.height], targetRect) + "px");
-		});
+		})
+		.attr("alt", this.config.get("alt"));
 
-  var targetURL = this.config.get("linkURL");
+	var targetURL = this.config.get("linkURL");
 
-  if (targetURL) {
-  	var a = $("<a></a>").attr("href", targetURL);
-  	element = element.wrap(a);
-  }
+	if (targetURL) {
+		var a = $("<a></a>").attr("href", targetURL);
+		element = element.wrap(a);
+	}
 	return element;
 };
 
