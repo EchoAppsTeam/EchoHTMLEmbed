488cc17495ff1bb291a87fbb8e45ad6246e3853b
Eugene Efimochkin <eugene@aboutecho.com>
2014-08-27 15:23:44 +0400

    Proper indentation

--- src/image/dashboard.js
+++ src/image/dashboard.js
@@ -9,51 +9,51 @@ dashboard.inherits = Echo.Utils.getComponent("Echo.AppServer.Dashboards.AppSetti
 
 dashboard.config = {
 	"ecl": [{
-			"name": "imageURL",
-			"type": "string",
-			"component": "Input",
-			"default": "",
-			"config": {
-				"title": "Image URL",
-				"desc": "Specifies URL the image will be loaded from"
-			}
-		}, {
-			"name": "height",
-			"type": "string",
-			"component": "Input",
-			"default": "",
-			"config": {
-				"title": "Height",
-				"desc": "Specifies width of the image"
-			}
-		}, {
-			"name": "width",
-			"type": "string",
-			"component": "Input",
-			"default": "",
-			"config": {
-				"title": "Width",
-				"desc": "Specifies height of the image"
-			}
-		}, {
-			"name": "alt",
-			"type": "string",
-			"component": "Input",
-			"default": "",
-			"config": {
-				"title": "Alt text",
-				"desc": "Sets the alt text for image"
-			}
-		}, {
-			"name": "linkURL",
-			"type": "string",
-			"component": "Input",
-			"default": "",
-			"config": {
-				"title": "Hyperlink URL",
-				"desc": "Sets the URL for hyperlink around the image"
-			}
-		}]
+		"name": "imageURL",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Image URL",
+			"desc": "Specifies URL the image will be loaded from"
+		}
+	}, {
+		"name": "height",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Height",
+			"desc": "Specifies height of the image"
+		}
+	}, {
+		"name": "width",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Width",
+			"desc": "Specifies width of the image"
+		}
+	}, {
+		"name": "alt",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Alt text",
+			"desc": "Sets the alt text for image"
+		}
+	}, {
+		"name": "linkURL",
+		"type": "string",
+		"component": "Input",
+		"default": "",
+		"config": {
+			"title": "Hyperlink URL",
+			"desc": "Sets the URL for hyperlink around the image"
+		}
+	}]
 };
 
 dashboard.init = function() {
