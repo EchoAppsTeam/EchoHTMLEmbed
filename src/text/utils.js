be26df1e7e80e25c07d28df03988485f53c9be98
Eugene Efimochkin <eugene@aboutecho.com>
2014-09-12 18:24:09 +0400

    Removing $().each cycle

--- src/text/utils.js
+++ src/text/utils.js
@@ -8,13 +8,15 @@ if (!window.Echo.Apps.Text.Utils) window.Echo.Apps.Text.Utils = {};
 Echo.Apps.Text.Utils.filterContent = function(content, whiteList) {
 		var sandbox = $('<div></div>').html(Echo.Utils.sanitize(content, "html"));
 
-		var sanitizeInPlace = function(DOMElement) {
+		var sanitizeInPlace = function(DOMElement, keepTop) {
 			var allowed, item, i;
 
 			for (i = 0; i < DOMElement.children.length; i++) {
 				item = DOMElement.children[i];
 				sanitizeInPlace(item);
 			}
+
+			if (keepTop) return; // only sanitize children, do not alter this one node
 
 			allowed = whiteList[DOMElement.localName];
 			if (!allowed) {
@@ -34,9 +36,7 @@ Echo.Apps.Text.Utils.filterContent = function(content, whiteList) {
 			}
 		};
 
-		sandbox.children().each(function() {
-			sanitizeInPlace(this);
-		});
+		sanitizeInPlace(sandbox.get(0), true);
 
 		content = sandbox.html();
 		return content;
