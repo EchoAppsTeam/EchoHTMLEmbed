ea826e9e43c199bcab26aa864516252678d95f1b
Alexandr Evseev <evseev@aboutecho.com>
2015-02-03 14:49:21 +0300

    Filter content before insert (to prevent xss)

--- grunt/config/concat.js
+++ grunt/config/concat.js
@@ -2,15 +2,15 @@ module.exports = {
 	dashboard: {
 		src: [
 			'<%= dirs.build %>/*/dashboard.js',
-			'<%= dirs.build %>/*/dashboard/*.js',
-			'<%= dirs.build %>/text/utils.js'
+			'<%= dirs.build %>/*/dashboard/*.js'
 		],
 		nonull: true,
 		dest: '<%= dirs.build %>/dashboards.js'
 	},
 	app: {
 		src: [
-			'<%= dirs.build %>/*/app.js'
+			'<%= dirs.build %>/*/app.js',
+			'<%= dirs.build %>/text/utils.js'
 		],
 		nonull: true,
 		dest: '<%= dirs.build %>/apps.js'
--- src/text/app.js
+++ src/text/app.js
@@ -6,11 +6,34 @@ var textApp = Echo.App.manifest("Echo.Apps.Text");
 if (Echo.App.isDefined("Echo.Apps.Text")) return;
 
 textApp.config = {
-	"content": ""
+	"content": "",
+	"filterOptions": {
+		"b": {},
+		"i": {},
+		"h1": {},
+		"h2": {},
+		"h3": {},
+		"h4": {},
+		"p": {},
+		"br": {},
+		"ul": {},
+		"ol": {},
+		"li": {},
+		"hr": {},
+		"a": {
+			"href": /^(https?\:)?\/\//
+		}
+	}
 };
 
 textApp.templates.main =
 	'<div class="{class:container}">{config:content}</div>';
+
+textApp.renderers.container = function(element) {
+	return element
+		.empty()
+		.append(Echo.Apps.Text.Utils.filterContent(this.config.get("content"), this.config.get("filterOptions")));
+};
 
 textApp.css =
 	".{class} { font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 15px; }";
